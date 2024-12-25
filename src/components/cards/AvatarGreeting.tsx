8c97ad151197752513e8c49b97681b8d206ed297
Anuj Vijay Patil <120624643+AnujPatil110377@users.noreply.github.com>
2024-12-25 23:47:10 +0530

    small size

--- src/components/cards/AvatarGreeting.tsx
+++ src/components/cards/AvatarGreeting.tsx
@@ -5,7 +5,7 @@ const AvatarGreeting = () => {
     <motion.div
       className="relative w-32 h-32"
       initial={{ scale: 1 }}
-      animate={{ scale: 2 }}
+      animate={{ scale: 1.5 }}
       transition={{ duration: 0.5 }}
     >
       <div className="w-32 h-32 rounded-full overflow-hidden flex items-center justify-center">
